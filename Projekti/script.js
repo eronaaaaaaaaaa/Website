ae6df90994c8b29512270e9792f4ca0b03ebcfe1
eronaaaaaaaaaa <eronagojnovcii@gmail.com>
2024-11-14 23:39:36 +0100

    made some changes

---
+++ Projekti/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const links = document.querySelectorAll('nav ul li a');
+    
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').slice(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetElement.offsetTop - 60,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
